9de2cacf4864f4f799c6c538bd71fc528f27d988
Konstantin Minakov <mikovic@yandex.ru>
2019-04-10 13:13:09 +0400

    дз №2 Работает хорошо

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -1,43 +1,24 @@
 package ru.geekbrains;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	Texture imgBackGround;
-	TextureRegion backGround;
+import ru.geekbrains.screen.MenuScreen;
+
+public class StarGame extends Game {
+
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+		setScreen(new MenuScreen());
 
-		img = new Texture("badlogic.jpg");
-		imgBackGround = new Texture("bike.png");
-		backGround = new TextureRegion(imgBackGround, 0, 0, 1920, 760);
-		backGround.getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 
 	}
 
-	@Override
-	public void render () {
-
-		Gdx.gl.glClearColor(1, 1,1, 1 );
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(backGround, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		batch.draw(img, 100, 100);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
 }
---
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -0,0 +1,81 @@
+package ru.geekbrains.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+import ru.geekbrains.base.BaseScreen;
+public class MenuScreen extends BaseScreen {
+    private Vector2 touch;
+    private Vector2 pos;
+    private Vector2 v;
+    protected Texture img;
+    protected double len;
+    protected  Vector2 vCpyNor;
+
+    @Override
+    public void show() {
+        super.show();
+        touch = new Vector2();
+        pos = new Vector2();
+        v = new Vector2(0.5f,0.6f);
+        len = v.len();
+        vCpyNor = v.cpy().nor();
+        img = new Texture("badlogic.jpg");
+    }
+
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        pos.add(v);
+        batch.begin();
+        batch.draw(img, pos.x, pos.y);
+        batch.end();
+        if(Math.abs(pos.x - touch.x) < Math.abs(v.x) && Math.abs(pos.y - touch.y) < Math.abs(v.y)) {
+            v.setZero();
+        }
+
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        img.dispose();
+    }
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        System.out.println("touchDown touch.x = " + touch.x + " touch.y = " + touch.y);
+        Vector2 dist = touch.cpy().sub(pos);
+        dist.nor();
+        double cosAngle = Math.abs(vCpyNor.dot(dist));
+        double projectionV = len * cosAngle;
+        v = dist.scl((float) projectionV);
+        return false;
+    }
+    @Override
+    public boolean keyDown(int keycode) {
+        System.out.println("keyDown keycode = " + keycode);
+        Vector2 right = new Vector2();
+        Vector2 up = new Vector2();
+        right.set(1, 0);
+        up.set(0, 1);
+        double angle = vCpyNor.dot(right); ;
+        switch(keycode) {
+            case 19:
+                v = up.scl((float)(len * Math.sin(angle)));
+
+                break;
+            case 20:
+                v = up.scl(-1).scl((float)(len * Math.sin(angle)));
+                break;
+            case 21:
+                v = right.scl(-1).scl(Math.abs(vCpyNor.dot(right)));
+                break;
+            case 22:
+                v = right.scl(Math.abs(vCpyNor.dot(right)));
+                break;
+        }
+        return false;
+    }
+}
